a01f1d508a95e1221163ff373a89be7985bc94ed
Martin Liska <martin.liska@hey.com>
2024-06-13 17:51:16 +0200

    Add documentation entry

--- src/lib.rs
+++ src/lib.rs
@@ -23,42 +23,61 @@ mod address_date_format {
 
 #[derive(Deserialize, Debug)]
 pub struct Address {
+    /// Kód adresního místa vedeného v Informačním systému územní identifikace (ISÚI).
     #[serde(rename = "Kód ADM")]
     pub adm_code: u32,
+    /// Kód obce vedené v ISÚI, ze které jsou vypsána všechna adresní místa.
     #[serde(rename = "Kód obce")]
     pub town_code: u32,
+    /// Název obce, ze které jsou vypsána všechna adresní místa.
     #[serde(rename = "Název obce")]
     pub town: String,
+    /// Kód městského obvodu/městské části, který je vyplněn pouze v případě členěných statutárních měst.
     #[serde(rename = "Kód MOMC")]
     pub city_part_code: Option<u64>,
+    /// Název městského obvodu/městské části, který je vyplněn pouze v případě členěných statutárních měst.
     #[serde(rename = "Název MOMC")]
     pub city_part: Option<String>,
+    /// Kód obvodu Prahy, který je vyplněn pouze v případě Hlavního města Prahy.
     #[serde(rename = "Kód obvodu Prahy")]
     pub prague_part_code: Option<u64>,
+    /// Název obvodu Prahy, který je vyplněn pouze v případě Hlavního města Prahy.
     #[serde(rename = "Název obvodu Prahy")]
     pub prague_part: Option<String>,
-    #[serde(rename = "Název části obce")]
-    pub town_part: String,
+    /// Kód části obce v rámci nadřazené obce, ve které je číslován stavební objekt.
     #[serde(rename = "Kód části obce")]
     pub town_part_code: u32,
+    /// Název části obce v rámci nadřazené obce, ve které je číslován stavební objekt.
+    #[serde(rename = "Název části obce")]
+    pub town_part: String,
+    /// Kód ulice, která je navázána na adresní místo. Může být vyplněn pouze u obcí, které mají zavedenu uliční síť.
     #[serde(rename = "Kód ulice")]
     pub street_code: Option<u32>,
+    /// Název ulice, která je navázána na adresní místo. Může být vyplněn pouze u obcí, které mají zavedenu uliční síť.
     #[serde(rename = "Název ulice")]
     pub street: Option<String>,
+    /// Typ stavebního objektu.
     #[serde(rename = "Typ SO")]
     pub object_type: String,
+    /// Číslo popisné nebo číslo evidenční, podle rozlišeného typu SO.
     #[serde(rename = "Číslo domovní")]
     pub number: u32,
+    /// Číslo orientační, slouží k orientaci v rámci nadřazené ulice.
     #[serde(rename = "Číslo orientační")]
     pub orientation_number: Option<u32>,
+    /// Znak čísla orientačního, uveden v případě, že je znak k orientačnímu číslu přidělen.
     #[serde(rename = "Znak čísla orientačního")]
     pub orientation_number_sign: Option<String>,
+    /// Poštovní směrovací číslo.
     #[serde(rename = "PSČ")]
     pub zip_code: u32,
+    /// Souřadnice X definičního bodu adresního místa v systému S-JTSK (systém jednotné trigonometrické sítě katastrální), uvedené v \[m\].
     #[serde(rename = "Souřadnice X")]
     pub location_x: Option<f32>,
+    /// Souřadnice Y definičního bodu adresního místa v systému S-JTSK (systém jednotné trigonometrické sítě katastrální), uvedené v \[m\].
     #[serde(rename = "Souřadnice Y")]
     pub location_y: Option<f32>,
+    /// Datum platnosti adresního místa ve tvaru RRRR-MM-DD. Pokud je datum 1. 7. 2011, jedná se o adresní místo vzniklé při úvodní migraci dat.
     #[serde(rename = "Platí Od", with = "address_date_format")]
     pub valid_since: DateTime<Utc>,
 }
@@ -73,6 +92,7 @@ pub enum AddressError {
     Csv(#[from] csv::Error),
 }
 
+/// Parses the Czech Republic addresses provided by [RUIAN](https://nahlizenidokn.cuzk.cz/StahniAdresniMistaRUIAN.aspx) in the ZIP format that contains CSV files.
 pub fn parse_addresses_from_csv(reader: impl Read + Seek) -> anyhow::Result<Vec<Address>> {
     let mut addresses = Vec::new();
     let mut zip = zip::ZipArchive::new(reader)?;
